dcc9996b55cf74e78788060081a8cfbfce0cd7a9
julie <21904615@unicaen.fr>
2021-02-23 13:34:16 +0100

    Ajout crudOrgas

---
+++ app/controllers/crud/viewers/CrudOrgasViewer.php
@@ -0,0 +1,10 @@
+<?php
+namespace controllers\crud\viewers;
+
+use Ubiquity\controllers\crud\viewers\ModelViewer;
+ /**
+  * Class CrudOrgasViewer
+  */
+class CrudOrgasViewer extends ModelViewer{
+	//use override/implement Methods
+}
