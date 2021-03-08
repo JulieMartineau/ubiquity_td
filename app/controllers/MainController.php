51d4c64c49caee18851230e43d5db89bc97d5911
julie <21904615@unicaen.fr>
2021-03-08 15:13:53 +0100

    Auth faite et fonctionnelle

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -1,14 +1,23 @@
 <?php
 namespace controllers;
 use Ubiquity\attributes\items\router\Route;
+use Ubiquity\controllers\auth\AuthController;
+use Ubiquity\controllers\auth\WithAuthTrait;
 
- /**
+/**
   * Controller MainController
   */
 class MainController extends ControllerBase{
-
-    #[Route('_default',name: 'home')]
+use WithAuthTrait;
+    #[Route('_default',name:'home')]
 	public function index(){
-		
+        $this->loadView("MainController/index.html");
 	}
+    public function initialize() {
+        parent::initialize();
+    }
+    protected function getAuthController(): AuthController {
+        return new MyAuth($this);
+    }
+
 }
