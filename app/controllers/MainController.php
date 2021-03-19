f92d4982412a53c3cb76c320d528fe31f4bc800f
julie <21904615@unicaen.fr>
2021-03-19 14:47:01 +0100

    Création de la route store

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -1,23 +1,37 @@
 <?php
 namespace controllers;
+use models\Order;
+use models\Product;
+use models\Section;
+use models\User;
 use Ubiquity\attributes\items\router\Route;
 use Ubiquity\controllers\auth\AuthController;
 use Ubiquity\controllers\auth\WithAuthTrait;
+use Ubiquity\orm\DAO;
 
 /**
   * Controller MainController
   */
 class MainController extends ControllerBase{
 use WithAuthTrait;
+
+
     #[Route('_default',name:'home')]
 	public function index(){
-        $this->loadView("MainController/index.html");
+        $promos=DAO::getAll(Product::class,'promotion<?', false, [0]);
+        $this->loadView("MainController/index.html", ["promos"=>$promos]);
 	}
+
     public function initialize() {
         parent::initialize();
     }
     protected function getAuthController(): AuthController {
         return new MyAuth($this);
+    }
+    #[Route('store',name:'store')]
+    public function store(){
+        $sections=DAO::getAll(Section::class,'', ['products']);
+        $this->loadView('MainController/store.html',['section'=>$sections]);
     }
 
 }
