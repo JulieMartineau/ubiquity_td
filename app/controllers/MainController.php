0fdf139bc91ff43b7c8088e34ad57abeefa8f084
julie <21904615@unicaen.fr>
2021-04-11 18:04:33 +0200

    Ajout des routes form

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -6,6 +6,7 @@ use models\Product;
 use models\Section;
 use models\User;
 use Ubiquity\attributes\items\router\Route;
+use Ubiquity\contents\validation\ValidatorsManager;
 use Ubiquity\controllers\auth\AuthController;
 use Ubiquity\controllers\auth\WithAuthTrait;
 use Ubiquity\orm\DAO;
@@ -73,4 +74,63 @@ use WithAuthTrait;
         $basket = $this->getBasket();
     }
 
+    #[Route('/form',name:'form')]
+    public function form(){
+        $this->jquery->renderView("MyController/form.html");
+    }
+    #[Route(path: "form/submit",name: "main.submit")]
+    public function submit(){
+        var_dump($_POST);
+    }
+    #[Route(path: "/htmlForm",name: "main.htmlForm")]
+    public function htmlForm(){
+        $frm=$this->jquery->semantic()->htmlForm('html-form');
+        $frm->setActionTarget(Router::path('main.submit'),'#response');
+        $frm->addInput('name','Name','text','')->addRule('empty');
+        $frm->addDropdown('user',UArrayModels::asKeyValues(DAO::getAll(User::class,'',false),'getId'),'User','no user');
+        $frm->addButton('bt','positive');
+        $frm->setValidationParams(['on'=>'blur','inline'=>true]);
+        $this->jquery->renderView('MainController/htmlForm.html');
+    }
+    #[Route(path: "/dataForm",name: "main.dataForm")]
+    public function dataForm(){
+        $u=new User();
+        $frm=$this->jquery->semantic()->dataForm('data-form',$u);
+        $frm->setValidationParams(['on'=>'blur','inline'=>true]);
+        $frm->setActionTarget(Router::path('main.addUser'),'#response');
+        $frm->setFields(['name','password','email','submit']);
+
+        $rules=ValidatorsManager::getUIConstraints($u);
+        $frm->fieldAsInput('password',['inputType'=>'password']+($rules['password']??[]));
+        $frm->fieldAsInput('name',$rules['name']??[]);
+        $frm->fieldAsInput('email',$rules['email']??[]);
+        $frm->fieldAsButton('submit','positive fluid',['value'=>'Valider']);
+
+        $this->jquery->renderView('MainController/dataForm.html');
+    }
+    #[Route(path: "/addUser",name: "main.addUser")]
+    public function addUser(){
+        $user=new User();
+        URequest::setValuesToObject($user);
+        if(DAO::insert($user)){
+            $message="Utilisateur $user ajouté.";
+            $type='success';
+        }else{
+            $message="Impossible d'ajouter $user";
+            $type='error';
+        }
+        $this->loadView('MainController/addUser.html',compact('message','type'));
+    }
+    #[Route(path: "/dt",name: "main.dt")]
+    public function dt(){
+        $products=DAO::getAll(Product::class,'1=1 order by idSection',['section']);
+        $dt=$this->jquery->semantic()->dataTable('dt',Product::class,$products);
+        $dt->setCompact(true);
+        $dt->setFields(['name','section','price','stock']);
+
+        $dt->fieldAsHeader('section');
+        $dt->fieldAsLabel('name');
+        $dt->addGroupBy('section');
+        $this->jquery->renderView('MainController/dt.html');
+    }
 }
